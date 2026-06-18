4ab1b5b60fd9f33b7879f5924e5f5ffdc8790236
Rishabh Thakur <rishabhthak@Rishabhs-MacBook-Air.local>
2026-06-17 22:05:55 -0700

    new layout

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/800.css";
 import "@fontsource/instrument-serif/400.css";
+import "@fontsource/instrument-serif/400-italic.css";
 import "@fontsource/jetbrains-mono/400.css";
 import "@fontsource/jetbrains-mono/500.css";
 import "@fontsource/jetbrains-mono/700.css";
